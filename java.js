a0b230342f071e65c6b275182f74beffadb8f135
vithor5 <vitor.camarapd@gmail.com>
2022-09-29 16:41:31 -0300

    NavBar Responsivo1

--- java.js
+++ java.js
@@ -20,6 +20,53 @@ function div_out(){
 <<<<<<< HEAD
 } 
 */
+
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+          link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                index / 7 + 0.3
+              }s`);
+        });
+      }
+    
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    "#resp-nav-menu",
+    ".nav-menu",
+    ".dropdown",
+  );
+  mobileNavbar.init();
+
+/*MENU INFERIOR*/
 var img_opt = document.getElementById("img_opt")
 var tit_opt = document.getElementById("terceira")
 var txt_opt1 = document.getElementById("txt_opt1")
